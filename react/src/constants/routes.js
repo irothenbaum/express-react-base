7398d0e6ba82ed60cd0480e407f966914b04052f
Isaac <irothenbaum@gmail.com>
2019-09-03 16:24:59 -0400

    Added the ConstructLink function

--- react/src/constants/routes.js
+++ react/src/constants/routes.js
@@ -1,3 +1,21 @@
 export const _BASE_PATH = '/'
 export const ROUTE_ROOT = '/'
-export const ROUTE_GAME = '/game/:id'
+export const ROUTE_GAME = '/game/:game_id'
+
+export const ConstructLink = (route, params) => {
+    let retVal = route
+    if (typeof params === 'object') {
+        Object.entries(params).forEach(tuple => {
+            // TODO: Should we check that they didn't send *extra* params for this route?
+            retVal = retVal.replace(':' + tuple[0], tuple[1])
+        })
+    }
+
+    // make sure we didn't miss any params
+    if (retVal.indexOf(':') !== -1) {
+        throw new Error("Invalid link created")
+    }
+
+    return retVal
+}
+
